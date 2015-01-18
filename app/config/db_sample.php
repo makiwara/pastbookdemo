639fcd5ef35b824b60056d44878ff4bb83e2e80c
Nikolay Yaremko <makiwara@humanemagica.com>
2015-01-18 05:05:33 +0500

    provided mysql-style db.php sample

---
+++ app/config/db_sample.php
@@ -0,0 +1,10 @@
+<?php
+
+$app['dbal'] = array(
+    'dbname' => 'pastbook',
+    'user' => 'USERNAME',
+    'password' => 'PASSWORD',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+
