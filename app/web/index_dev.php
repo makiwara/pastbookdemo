bc4100ec307e2dca4f5eb52d21d1460d4c1786b9
Nikolay Yaremko <makiwara@humanemagica.com>
2015-01-20 02:24:36 +0500

    setup for localhost development on silex

--- app/web/index_dev.php
+++ app/web/index_dev.php
@@ -17,6 +17,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 Debug::enable();
 
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../config/dev.php';
 require __DIR__.'/../src/controllers.php';
